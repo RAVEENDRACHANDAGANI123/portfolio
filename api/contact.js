17a6d91a7c245a9a1ee699c3bb84e56267df28ca
CHANDAGANI _RAVEENDRA <121965552+RAVEENDRACHANDAGANI123@users.noreply.github.com>
2024-08-25 19:47:04 +0530

    Update contact.js
    Signed-off-by: CHANDAGANI _RAVEENDRA  <121965552+RAVEENDRACHANDAGANI123@users.noreply.github.com>

--- api/contact.js
+++ api/contact.js
@@ -1,34 +1,40 @@
-const { google } = require('googleapis');
+import { google } from 'googleapis';
 
+const oauth2Client = new google.auth.OAuth2(
+  '914159560192-oe8ofrq4tqpjc9epdsrjvv03rrg31vvq.apps.googleusercontent.com', // Your client ID
+  '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', // Your client secret
+  'https://portfolio-ckpr9pbzy-raveendra-chandaganis-projects.vercel.app' // Your redirect URI
+);
 
-// Initialize the Google Sheets API with your API key
+// Set the tokens
+oauth2Client.setCredentials({
+  access_token: '4/0AQlEd8xu_l_dITNbojodUuz5fb4oQRJtY35KjqEy6ly0JvrHuzhGTQiEucnafjRgK-looA', // Your access token
+  refresh_token: '1//0gHQYIYXDHfYhCgYIARAAGBASNwF-L9Ir6Xm4gOxSeh7_h5qKUFpCOB20gzuCWWXuhT7bz0mJhAxP-aEoLFJBBb-aJ2K-8a5stOQ' // Your refresh token
+});
+
 const sheets = google.sheets({
   version: 'v4',
-  auth: 'AIzaSyAsOYE6zKPE7qaRGwQcZdbFHQQQogY1aK0', // Your API Key
+  auth: oauth2Client
 });
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Extract the name, email, and message from the request body
     const { name, email, message } = req.body;
 
-    // Check for missing fields
     if (!name || !email || !message) {
       return res.status(400).json({ error: 'Missing fields' });
     }
 
-    // Set up the request to append data to Google Sheets
     const request = {
-      spreadsheetId: '164VbM_FV1LBa6fysopTjx7iofd1PqLgBGsg2vkbUgyw', // Your Google Sheet ID
-      range: 'Sheet1!A1:C1', // Specify the range to append the data
+      spreadsheetId: '164VbM_FV1LBa6fysopTjx7iofd1PqLgBGsg2vkbUgyw', // Your spreadsheet ID
+      range: 'Sheet1!A1:C1',
       valueInputOption: 'RAW',
       resource: {
-        values: [[name, email, message]], // The data to append
+        values: [[name, email, message]],
       },
     };
 
     try {
-      // Append data to the Google Sheets
       await sheets.spreadsheets.values.append(request);
       return res.status(200).json({ message: 'Message stored!' });
     } catch (error) {
@@ -36,7 +42,6 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: 'Failed to store message' });
     }
   } else {
-    // Handle unsupported methods
     res.status(405).json({ error: 'Method not allowed' });
   }
 }
