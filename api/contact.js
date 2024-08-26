e5742b00f19a310e9f0da1702ef640654403a188
CHANDAGANI _RAVEENDRA <121965552+RAVEENDRACHANDAGANI123@users.noreply.github.com>
2024-08-26 08:03:17 +0530

    Update contact.js
    Signed-off-by: CHANDAGANI _RAVEENDRA  <121965552+RAVEENDRACHANDAGANI123@users.noreply.github.com>

--- api/contact.js
+++ api/contact.js
@@ -1,46 +1,4 @@
-import { google } from 'googleapis';
-
-const oauth2Client = new google.auth.OAuth2(
-  '914159560192-oe8ofrq4tqpjc9epdsrjvv03rrg31vvq.apps.googleusercontent.com', // Your client ID
-  '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', // Your client secret
-  'https://portfolio-ckpr9pbzy-raveendra-chandaganis-projects.vercel.app' // Your redirect URI
-);
-
-// Set the tokens
-oauth2Client.setCredentials({
-  access_token: '4/0AQlEd8xu_l_dITNbojodUuz5fb4oQRJtY35KjqEy6ly0JvrHuzhGTQiEucnafjRgK-looA', // Your access token
-  refresh_token: '1//0gHQYIYXDHfYhCgYIARAAGBASNwF-L9Ir6Xm4gOxSeh7_h5qKUFpCOB20gzuCWWXuhT7bz0mJhAxP-aEoLFJBBb-aJ2K-8a5stOQ' // Your refresh token
-});
-
-const sheets = google.sheets({
-  version: 'v4',
-  auth: oauth2Client
-});
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, message } = req.body;
-
-    if (!name || !email || !message) {
-      return res.status(400).json({ error: 'Missing fields' });
-    }
-
-     // Get current date and time in IST
-    const now = new Date();
-    const istOffset = 5 * 60 * 60 * 1000 + 30 * 60 * 1000; // IST offset in milliseconds (5 hours 30 minutes)
-    const istTime = new Date(now.getTime() + istOffset).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
-
-
-    const request = {
-      spreadsheetId: '164VbM_FV1LBa6fysopTjx7iofd1PqLgBGsg2vkbUgyw', // Your spreadsheet ID
-      range: 'Sheet1!A1:D1',
-      valueInputOption: 'RAW',
-      resource: {
-        values: [[name, email, message, istTime]],
-      },
-    };
-
-   document.getElementById('contact-form').addEventListener('submit', async function(e) {
+document.getElementById('contactForm').addEventListener('submit', async function(e) {
   e.preventDefault();
 
   // Form data
@@ -80,6 +38,3 @@ export default async function handler(req, res) {
 document.querySelector('.popup-close').addEventListener('click', function() {
   document.getElementById('popup-message').style.display = 'none';
 });
-
-  }
-}
